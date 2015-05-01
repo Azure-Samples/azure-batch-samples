5d1965c32a73e6b4c937b9b2f440cae0b006822f
Fred Park <fred.park@microsoft.com>
2015-04-30 19:19:04 -0700

    Add page blob support

--- Python/Storage/README.md
+++ Python/Storage/README.md
@@ -1,7 +1,8 @@
-###blobxfer.py
-Please refer to this blog post for more information:
-http://blogs.technet.com/b/windowshpc/archive/2015/04/16/linux-blob-transfer-python-code-sample.aspx
+##blobxfer.py
+Please refer to the Microsoft HPC and Azure Batch Team [blog post](http://blogs.technet.com/b/windowshpc/archive/2015/04/16/linux-blob-transfer-python-code-sample.aspx)
+for code sample explanations.
 
+###Introduction
 The blobxfer.py script allows interacting with storage accounts using any of
 the following methods: (1) management certificate, (2) shared account key,
 (3) SAS key. The script can, in addition to working with single files, mirror
@@ -23,10 +24,16 @@ setup.py procedures.
 Program parameters and command-line options can be listed via the -h switch. At
 the minimum, three positional arguments are required: storage account name,
 container name, local resource. Additionally, one of the following
-authentication switches must be supplied: --subscriptionid with
---managementcert, --storageaccountkey, or --saskey. It is recommended to use
-SAS keys wherever possible; only HTTPS transport is used in the script.
+authentication switches must be supplied: `--subscriptionid` with
+`--managementcert`, `--storageaccountkey`, or `--saskey`. It is recommended
+to use SAS keys wherever possible; only HTTPS transport is used in the script.
 
+Please remember when using SAS keys that only container-level SAS keys will
+allow for entire directory uploading or container downloading. The container
+must also have been created beforehand, as containers cannot be created
+using SAS keys.
+
+###Example Usage
 The script will attempt to perform a smart transfer, by detecting if the local
 resource exists. For example:
 
@@ -38,7 +45,7 @@ If mylocalfile.txt exists locally, then the script will attempt to upload the
 file to container0 on mystorageacct. If the file does not exist, then it will
 attempt to download the resource. If the desired behavior is to download the
 file from Azure even if the local file exists, one can override the detection
-mechanism with --forcedownload. --forceupload is available to force the
+mechanism with `--forcedownload`. `--forceupload` is available to force the
 transfer to Azure storage. Note that you may use the --remoteresource flag to
 rename the local file as the blob name on Azure storage if uploading.
 
@@ -46,7 +53,7 @@ If the local resource is a directory that exists, the script will attempt to
 mirror (recursively copy) the entire directory to Azure storage while
 maintaining subdirectories as virtual directories in Azure storage. You can
 disable the recursive copy (i.e., upload only the files in the directory)
-using the --no-recursive flag.
+using the `--no-recursive` flag.
 
 To download an entire container from your storage account, an example
 commandline would be:
@@ -57,24 +64,42 @@ blobxfer.py mystorageacct container0 mylocaldir --remoteresource .
 
 Assuming mylocaldir directory does not exist, the script will attempt to
 download all of the contents in container0 because “.” is set with
---remoteresource flag. To download individual blobs, one would specify the
-blob name instead of “.” with the --remoteresource flag. If mylocaldir
+`--remoteresource` flag. To download individual blobs, one would specify the
+blob name instead of “.” with the `--remoteresource` flag. If mylocaldir
 directory exists, the script will attempt to upload the directory instead of
 downloading it. In this case, if you want to force the download, indicate that
-with --forcedownload. When downloading an entire container, the script will
+with `--forcedownload`. When downloading an entire container, the script will
 attempt to pre-allocate file space and recreate the sub-directory structure
 as needed.
 
-As of version 0.9.0, the script will automatically default to skipping files
-where if the MD5 checksum of either the local file or the stored MD5 of the
-remote resource respectively matches the remote resource or local file, then
-the upload or download for the file will be skipped. This capability will
-allow one to perform rsync-like operations where only files that have changed
-will be transferred. This behavior can be forcefully disabled by specifying
---no-skiponmatch.
+###Notes
+A note on performance with Python versions < 2.7.9 (i.e., interpreter found
+on default Ubuntu 14.04 installations) -- as of requests 2.6.0, if certain
+packages are installed, as those found in `requests[security]` then the
+underlying urllib3 package will utilize the ndg-httpsclient package which
+will use [pyOpenSSL](https://urllib3.readthedocs.org/en/latest/security.html#pyopenssl).
+This will ensure the peers are [fully validated](https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning).
+However, this incurs a rather larger performance penalty. If you understand
+the potential security risks for disabling this behavior due to high
+performance requirements, you can either remove ndg-httpsclient or use the
+script in a virtualenv environment without the ndg-httpsclient package.
+Python versions >= 2.7.9 are not affected by this issue.
 
-Please remember when using SAS keys that only container-level SAS keys will
-allow for entire directory uploading or container downloading. The container
-must also have been created beforehand, as containers cannot be created
-using SAS keys.
+###Change Log
+* 0.9.1: the script supports page blob uploading. To specify local files to
+upload as page blobs, specify the `--pageblob` parameter. The script also has
+a feature to detect files ending in the `.vhd` extension and will
+automatically upload just these files as page blobs while uploading other
+files as block blobs. Specify the `--autovhd` parameter (without the
+`--pageblob` parameter) to enable this behavior.
+
+* 0.9.0: the script will automatically default to skipping files where if the
+MD5 checksum of either the local file or the stored MD5 of the remote resource
+respectively matches the remote resource or local file, then the upload or
+download for the file will be skipped. This capability will allow one to
+perform rsync-like operations where only files that have changed will be
+transferred. This behavior can be forcefully disabled by specifying
+`--no-skiponmatch`.
+
+* 0.8.2: performance regression fixes
 
--- Python/Storage/test/test_blobxfer.py
+++ Python/Storage/test/test_blobxfer.py
@@ -36,6 +36,12 @@ def test_compute_md5(tmpdir):
     with pytest.raises(IOError):
         blobxfer.compute_md5_for_file_asbase64(testdata)
 
+def test_page_align_content_length():
+    assert 0 == blobxfer.page_align_content_length(0)
+    assert 512 == blobxfer.page_align_content_length(511)
+    assert 512 == blobxfer.page_align_content_length(512)
+    assert 1024 == blobxfer.page_align_content_length(513)
+
 @patch('time.sleep', return_value=None)
 def test_azure_request(patched_time_sleep):
     socket_error = socket.error()
@@ -64,7 +70,7 @@ def _func_raise_requests_exception_once(val, timeout=None):
         response.raise_for_status = lambda: None
         return response
     val.append(0)
-    ex = requests.exceptions.ConnectTimeout()
+    ex = requests.Timeout()
     raise ex
 
 @patch('time.sleep', return_value=None)
@@ -81,8 +87,8 @@ def test_http_request_wrapper(patched_time_sleep):
         mock.__name__ = 'name'
         blobxfer.http_request_wrapper(mock, timeout=0.001)
 
-    with pytest.raises(requests.exceptions.HTTPError):
-        exc = requests.exceptions.HTTPError()
+    with pytest.raises(requests.HTTPError):
+        exc = requests.HTTPError()
         exc.response = MagicMock()
         exc.response.status_code = 404
         mock = Mock(side_effect=exc)
@@ -184,6 +190,42 @@ def test_sasblobservice_putblocklist():
         with pytest.raises(IOError):
             sbs.put_block_list('container', 'blob', ['1', '2'], 'md5')
 
+def test_sasblobservice_setblobproperties():
+    session = requests.Session()
+    adapter = requests_mock.Adapter()
+    session.mount('mock', adapter)
+
+    with requests_mock.mock() as m:
+        m.put('mock://blobepcontainer/blobsaskey', status_code=200)
+        sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
+        try:
+            sbs.set_blob_properties('container', 'blob', ['1', '2'], 'md5')
+        except:
+            pytest.fail('unexpected Exception raised')
+
+        m.put('mock://blobepcontainer/blobsaskey', text='', status_code=201)
+        sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
+        with pytest.raises(IOError):
+            sbs.set_blob_properties('container', 'blob', ['1', '2'], 'md5')
+
+def test_sasblobservice_putblob():
+    session = requests.Session()
+    adapter = requests_mock.Adapter()
+    session.mount('mock', adapter)
+
+    with requests_mock.mock() as m:
+        m.put('mock://blobepcontainer/blobsaskey', status_code=201)
+        sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
+        try:
+            sbs.put_blob('container', 'blob', None, 'PageBlob', 'md5', 4)
+        except:
+            pytest.fail('unexpected Exception raised')
+
+        m.put('mock://blobepcontainer/blobsaskey', content='', status_code=200)
+        sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
+        with pytest.raises(IOError):
+            sbs.put_blob('container', 'blob', None, 'PageBlob', 'md5', 4)
+
 def test_blobchunkworker_run(tmpdir):
     lpath = str(tmpdir.join('test.tmp'))
     with open(lpath, 'wt') as f:
@@ -196,6 +238,10 @@ def test_blobchunkworker_run(tmpdir):
         'container', 'blob', 'blockid', 0, 4, flock, None))
     sa_in_queue.put((False, lpath, 'blobep', 'saskey',
         'container', 'blob', 'blockid', 0, 4, flock, None))
+    args = MagicMock()
+    args.pageblob = False
+    args.autovhd = False
+    args.timeout = None
 
     session = requests.Session()
     adapter = requests_mock.Adapter()
@@ -204,9 +250,9 @@ def test_blobchunkworker_run(tmpdir):
         m.put('mock://blobepcontainer/blobsaskey', status_code=201)
         sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
         bcw = blobxfer.BlobChunkWorker(exc_list, sa_in_queue, sa_out_queue,
-                sbs, None)
+                args, sbs)
         try:
-            bcw.putblock(lpath, 'container', 'blob', 'blockid', 0, 4, flock, None)
+            bcw.putblobdata(lpath, 'container', 'blob', 'blockid', 0, 4, flock, None)
         except:
             pytest.fail('unexpected Exception raised')
 
@@ -219,6 +265,22 @@ def test_blobchunkworker_run(tmpdir):
         m.get('mock://blobepcontainer/blobsaskey', status_code=201)
         bcw.run()
         assert len(exc_list) > 0
+
+    exc_list = []
+    sa_in_queue = queue.Queue()
+    sa_out_queue = queue.Queue()
+    sa_in_queue.put((True, lpath, 'blobep', 'saskey',
+        'container', 'blob', 'blockid', 0, 4, flock, None))
+    sa_in_queue.put((False, lpath, 'blobep', 'saskey',
+        'container', 'blob', 'blockid', 0, 4, flock, None))
+    args.pageblob = True
+    with requests_mock.mock() as m:
+        m.put('mock://blobepcontainer/blobsaskey', status_code=200)
+        sbs = blobxfer.SasBlobService('mock://blobep', 'saskey', None)
+        bcw = blobxfer.BlobChunkWorker(exc_list, sa_in_queue, sa_out_queue,
+                args, sbs)
+        with pytest.raises(IOError):
+            bcw.putblobdata(lpath, 'container', 'blob', 'blockid', 0, 4, flock, None)
 
 @patch('blobxfer.azure_request', return_value=None)
 def test_generate_xferspec_download_invalid(patched_azure_request):
@@ -329,6 +391,8 @@ def test_main(patched_parseargs, tmpdir):
     args.container = 'container'
     args.storageaccountkey = 'saskey'
     args.chunksizebytes = 5
+    args.pageblob = False
+    args.autovhd = False
     patched_parseargs.return_value = args
     with pytest.raises(ValueError):
         blobxfer.main()
@@ -365,6 +429,12 @@ def test_main(patched_parseargs, tmpdir):
     with pytest.raises(ValueError):
         blobxfer.main()
     args.subscriptionid = '1234'
+    args.pageblob = True
+    args.autovhd = True
+    with pytest.raises(ValueError):
+        blobxfer.main()
+    args.pageblob = False
+    args.autovhd = False
     with patch('azure.servicemanagement.ServiceManagementService') as mock:
         mock.return_value = MagicMock()
         mock.return_value.get_storage_account_keys = _mock_get_storage_account_keys
@@ -440,19 +510,37 @@ def test_main(patched_parseargs, tmpdir):
         m.put('https://blobep.blobep/container' + lpath + 'saskey?comp=blocklist',
                 status_code=201)
         m.put('https://blobep.blobep/container/' + notmp_lpath + \
-                'saskey?comp=block&blockid=00000000',
-                status_code=200)
+                'saskey?comp=block&blockid=00000000', status_code=200)
         m.put('https://blobep.blobep/container/' + notmp_lpath + \
-                'saskey?comp=blocklist',
-                status_code=201)
+                'saskey?comp=blocklist', status_code=201)
         with pytest.raises(SystemExit):
             blobxfer.main()
 
         args.recursive = False
-        m.put('https://blobep.blobep/container/blob.blobtmpsaskey?comp=blocklist', status_code=201)
-        m.put('https://blobep.blobep/container/test.tmp.blobtmpsaskey?comp=blocklist', status_code=201)
+        m.put('https://blobep.blobep/container/blob.blobtmpsaskey?comp=blocklist',
+                status_code=201)
+        m.put('https://blobep.blobep/container/test.tmp.blobtmpsaskey?comp=blocklist',
+                status_code=201)
+        m.put('https://blobep.blobep/container/blob.blobtmpsaskey?comp=block&blockid=00000000',
+                status_code=200)
         with pytest.raises(SystemExit):
             blobxfer.main()
+
+        m.put('https://blobep.blobep/container/blob.blobtmpsaskey', status_code=201)
+        m.put('https://blobep.blobep/container/test.tmpsaskey', status_code=201)
+        m.put('https://blobep.blobep/container/blob.blobtmpsaskey?comp=properties', status_code=200)
+        m.put('https://blobep.blobep/container/test.tmpsaskey?comp=properties', status_code=200)
+        args.pageblob = True
+        args.forceupload = True
+        args.forcedownload = False
+        blobxfer.main()
+        m.put('https://blobep.blobep/container/' + notmp_lpath + \
+                'saskey?comp=blocklist', status_code=201)
+        args.pageblob = False
+        blobxfer.main()
+        args.pageblob = False
+        args.autovhd = True
+        blobxfer.main()
 
     args.saskey = None
     args.storageaccountkey = 'key'
@@ -460,5 +548,9 @@ def test_main(patched_parseargs, tmpdir):
     with patch('azure.storage.BlobService') as mock:
         mock.return_value = MagicMock()
         mock.return_value.create_container = _mock_blobservice_create_container
+        blobxfer.main()
+        args.createcontainer = False
+        args.pageblob = True
+        args.autovhd = False
         blobxfer.main()
 
