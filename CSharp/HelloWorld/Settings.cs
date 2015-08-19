98f6b0cb992f881b4a23880c8b924d572b50a211
matthchr <matthchr@microsoft.com>
2015-08-19 15:45:34 -0700

    Update HelloWorld sample to be async
     - Clean up and refactor code some to be easier to read/understand.
     - Removed unnecessary task addition methods which added confusion
       to the code.
     - Add option ShouldDeleteJob for debugging purposes.
     - Move sample to a more correct namespace.

---
+++ CSharp/HelloWorld/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Microsoft.Azure.Batch.Samples.HelloWorld
+{
+    public partial class Settings
+    {
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            AddSetting(stringBuilder, "BatchAccountName", this.BatchAccountName);
+            AddSetting(stringBuilder, "BatchAccountKey", this.BatchAccountKey);
+            AddSetting(stringBuilder, "BatchServiceUrl", this.BatchServiceUrl);
+
+            AddSetting(stringBuilder, "StorageAccountName", this.StorageAccountName);
+            AddSetting(stringBuilder, "StorageAccountKey", this.StorageAccountKey);
+            AddSetting(stringBuilder, "StorageBlobEndpoint", this.StorageBlobEndpoint);
+
+            AddSetting(stringBuilder, "PoolId", this.PoolId);
+            AddSetting(stringBuilder, "PoolTargetNodeCount", this.PoolTargetNodeCount);
+            AddSetting(stringBuilder, "PoolOSFamily", this.PoolOSFamily);
+            AddSetting(stringBuilder, "PoolNodeVirtualMachineSize", this.PoolNodeVirtualMachineSize);
+            AddSetting(stringBuilder, "ShouldDeletePool", this.ShouldDeletePool);
+            AddSetting(stringBuilder, "ShouldDeleteJob", this.ShouldDeleteJob);
+            
+            return stringBuilder.ToString();
+        }
+
+        private static void AddSetting(StringBuilder stringBuilder, string settingName, object settingValue)
+        {
+            stringBuilder.AppendFormat("{0} = {1}", settingName, settingValue).AppendLine();
+        }
+    }
+}
