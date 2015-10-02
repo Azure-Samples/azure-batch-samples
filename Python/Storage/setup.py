7f940bf5a972dfd6c8c3d4900cde185e0a0fe29a
Fred Park <fred.park@microsoft.com>
2015-10-02 08:12:17 -0700

    Update blobxfer setup.py for Python 3.5

--- Python/Storage/setup.py
+++ Python/Storage/setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities',
     ],
     keywords='azcopy azure storage blob transfer copy',
