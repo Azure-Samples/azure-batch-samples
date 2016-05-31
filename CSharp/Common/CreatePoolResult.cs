dd393a9f4fff12ff865113b9a85c0fac1d67d12c
Ivan Towlson <ivan.towlson@microsoft.com>
2016-06-01 11:52:56 +1200

    Files containing enum types are unlikely ever to need using directives

--- CSharp/Common/CreatePoolResult.cs
+++ CSharp/Common/CreatePoolResult.cs
@@ -2,12 +2,6 @@
 
 namespace Microsoft.Azure.Batch.Samples.Common
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     public enum CreatePoolResult
     {
         PoolExisted,
