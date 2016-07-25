Azure-Samples/azure-batch-samples

commit 13081e3356db493b88c8248ebde7c0cf880c419d

MPI github samples added (#163)
* MPI github samples added

Python/Batch/common/helpers.py
@@ -25,6 +25,7 @@
 from __future__ import print_function
 import datetime
 import io
+import os
 import time
 
 import azure.storage.blob as azureblob
@@ -214,6 +215,54 @@ def read_compute_node_file_as_string(
     return _read_stream_as_string(stream, encoding)
 
 
+def get_vm_config_for_distro(batch_service_client, distro, version):
+    """
+    Gets a virtual machine configuration for the specified distro and version
+    from the list of Azure Virtual Machines Marketplace images verified to be
+    compatible with the Batch service.
+
+    :param batch_service_client: A Batch service client.
+    :type batch_service_client: `azure.batch.BatchServiceClient`
+    :param str distro: The Linux distribution that should be installed on the
+    compute nodes, e.g. 'Ubuntu' or 'CentOS'. Supports partial string matching.
+    :param str version: The version of the operating system for the compute
+    nodes, e.g. '15' or '14.04'. Supports partial string matching.
+    :rtype: `azure.batch.models.VirtualMachineConfiguration`
+    :return: A virtual machine configuration specifying the Virtual Machines
+    Marketplace image and node agent SKU to install on the compute nodes in
+    a pool.
+    """
+    # Get the list of node agents from the Batch service
+    node_agent_skus = batch_service_client.account.list_node_agent_skus()
+
+    # Get the first node agent that is compatible with the specified distro.
+    # Note that 'distro' in this case actually maps to the 'offer' property of
+    # the ImageReference.
+    node_agent = next(agent for agent in node_agent_skus
+                      for image_ref in agent.verified_image_references
+                      if distro.lower() in image_ref.offer.lower() and
+                      version.lower() in image_ref.sku.lower())
+
+    # Get the last image reference from the list of verified references
+    # for the node agent we obtained. Typically, the verified image
+    # references are returned in ascending release order so this should give us
+    # the newest image.
+    img_ref = [image_ref for image_ref in node_agent.verified_image_references
+               if distro.lower() in image_ref.offer.lower() and
+               version.lower() in image_ref.sku.lower()][-1]
+
+    # Create the VirtualMachineConfiguration, specifying the VM image
+    # reference and the Batch node agent to be installed on the node.
+    # Note that these commands are valid for a pool of Ubuntu-based compute
+    # nodes, and that you may need to adjust the commands for execution
+    # on other distros.
+    vm_config = batchmodels.VirtualMachineConfiguration(
+        image_reference=img_ref,
+        node_agent_sku_id=node_agent.id)
+
+    return vm_config
+
+
 def create_pool_if_not_exist(batch_client, pool):
     """Creates the specified pool if it doesn't already exist
 
@@ -231,6 +280,31 @@ def create_pool_if_not_exist(batch_client, pool):
             raise
         else:
             print("Pool {!r} already exists".format(pool.id))
+
+
+def create_job(batch_service_client, job_id, pool_id):
+    """
+    Creates a job with the specified ID, associated with the specified pool.
+
+    :param batch_service_client: A Batch service client.
+    :type batch_service_client: `azure.batch.BatchServiceClient`
+    :param str job_id: The ID for the job.
+    :param str pool_id: The ID for the pool.
+    """
+    print('Creating job [{}]...'.format(job_id))
+
+    job = batchmodels.JobAddParameter(
+        job_id,
+        batchmodels.PoolInformation(pool_id=pool_id))
+
+    try:
+        batch_service_client.job.add(job)
+    except batchmodels.batch_error.BatchErrorException as err:
+        print_batch_exception(err)
+        if err.error.code != "JobExists":
+            raise
+        else:
+            print("Job {!r} already exists".format(job_id))
 
 
 def wait_for_all_nodes_state(batch_client, pool, node_state):
@@ -266,7 +340,8 @@ def wait_for_all_nodes_state(batch_client, pool, node_state):
 
 
 def create_container_and_create_sas(
-        block_blob_client, container_name, permission, expiry=None):
+        block_blob_client, container_name, permission, expiry=None,
+        timeout=30):
     """Create a blob sas token
 
     :param block_blob_client: The storage block blob client to use.
@@ -274,11 +349,14 @@ def create_container_and_create_sas(
     :param str container_name: The name of the container to upload the blob to.
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
+    :param int timeout: timeout in minutes from now for expiry,
+                        will only be used if expiry is not specified
     :return: A SAS token
     :rtype: str
     """
     if expiry is None:
-        expiry = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        expiry = datetime.datetime.utcnow() + \
+                 datetime.timedelta(minutes=timeout)
 
     block_blob_client.create_container(
         container_name,
@@ -289,7 +367,8 @@ def create_container_and_create_sas(
 
 
 def create_sas_token(
-        block_blob_client, container_name, blob_name, permission, expiry=None):
+        block_blob_client, container_name, blob_name, permission, expiry=None,
+        timeout=30):
     """Create a blob sas token
 
     :param block_blob_client: The storage block blob client to use.
@@ -298,17 +377,21 @@ def create_sas_token(
     :param str blob_name: The name of the blob to upload the local file to.
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
+    :param int timeout: timeout in minutes from now for expiry,
+                        will only be used if expiry is not specified
     :return: A SAS token
     :rtype: str
     """
     if expiry is None:
-        expiry = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        expiry = datetime.datetime.utcnow() + \
+                 datetime.timedelta(minutes=timeout)
     return block_blob_client.generate_blob_shared_access_signature(
         container_name, blob_name, permission=permission, expiry=expiry)
 
 
 def upload_blob_and_create_sas(
-        block_blob_client, container_name, blob_name, file_name, expiry):
+        block_blob_client, container_name, blob_name, file_name, expiry,
+        timeout):
     """Uploads a file from local disk to Azure Storage and creates
     a SAS for it.
 
@@ -319,6 +402,8 @@ def upload_blob_and_create_sas(
     :param str file_name: The name of the local file to upload.
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
+    :param int timeout: timeout in minutes from now for expiry,
+                        will only be used if expiry is not specified
     :return: A SAS URL to the blob with the specified expiry time.
     :rtype: str
     """
@@ -336,7 +421,8 @@ def upload_blob_and_create_sas(
         container_name,
         blob_name,
         permission=azureblob.BlobPermissions.READ,
-        expiry=expiry)
+        expiry=expiry,
+        timeout=timeout)
 
     sas_url = block_blob_client.make_blob_url(
         container_name,
@@ -344,6 +430,62 @@ def upload_blob_and_create_sas(
         sas_token=sas_token)
 
     return sas_url
+
+
+def upload_file_to_container(block_blob_client, container_name, file_path,
+                             timeout):
+    """
+    Uploads a local file to an Azure Blob storage container.
+
+    :param block_blob_client: A blob service client.
+    :type block_blob_client: `azure.storage.blob.BlockBlobService`
+    :param str container_name: The name of the Azure Blob storage container.
+    :param str file_path: The local path to the file.
+    :param int timeout: timeout in minutes from now for expiry,
+                        will only be used if expiry is not specified
+    :rtype: `azure.batch.models.ResourceFile`
+    :return: A ResourceFile initialized with a SAS URL appropriate for Batch
+    tasks.
+    """
+    blob_name = os.path.basename(file_path)
+    print('Uploading file {} to container [{}]...'.format(file_path,
+                                                          container_name))
+    sas_url = upload_blob_and_create_sas(block_blob_client, container_name,
+                                         blob_name, file_path, expiry=None,
+                                         timeout=timeout)
+    return batchmodels.ResourceFile(file_path=blob_name,
+                                    blob_source=sas_url)
+
+
+def download_blob_from_container(block_blob_client,
+                                 container_name,
+                                 blob_name,
+                                 directory_path):
+    """
+    Downloads specified blob from the specified Azure Blob storage container.
+
+    :param block_blob_client: A blob service client.
+    :type block_blob_client: `azure.storage.blob.BlockBlobService`
+    :param container_name: The Azure Blob storage container from which to
+     download file.
+    :param blob_name: The name of blob to be downloaded
+    :param directory_path: The local directory to which to download the file.
+    """
+    print('Downloading result file from container [{}]...'.format(
+        container_name))
+
+    destination_file_path = os.path.join(directory_path, blob_name)
+
+    block_blob_client.get_blob_to_path(container_name,
+                                       blob_name,
+                                       destination_file_path)
+
+    print('  Downloaded blob [{}] from container [{}] to {}'.format(
+         blob_name,
+         container_name,
+         destination_file_path))
+
+    print('  Download complete!')
 
 
 def generate_unique_resource_name(resource_prefix):
@@ -356,6 +498,55 @@ def generate_unique_resource_name(resource_prefix):
     """
     return resource_prefix + "-" + \
         datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+
+
+def query_yes_no(question, default="yes"):
+    """
+    Prompts the user for yes/no input, displaying the specified question text.
+
+    :param str question: The text of the prompt for input.
+    :param str default: The default if the user hits <ENTER>. Acceptable values
+    are 'yes', 'no', and None.
+    :rtype: str
+    :return: 'yes' or 'no'
+    """
+    valid = {'y': 'yes', 'n': 'no'}
+    if default is None:
+        prompt = ' [y/n] '
+    elif default == 'yes':
+        prompt = ' [Y/n] '
+    elif default == 'no':
+        prompt = ' [y/N] '
+    else:
+        raise ValueError("Invalid default answer: '{}'".format(default))
+
+    while 1:
+        choice = input(question + prompt).lower()
+        if default and not choice:
+            return default
+        try:
+            return valid[choice[0]]
+        except (KeyError, IndexError):
+            print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+
+def print_batch_exception(batch_exception):
+    """
+    Prints the contents of the specified Batch exception.
+
+    :param batch_exception:
+    """
+    print('-------------------------------------------')
+    print('Exception encountered:')
+    if batch_exception.error and \
+            batch_exception.error.message and \
+            batch_exception.error.message.value:
+        print(batch_exception.error.message.value)
+        if batch_exception.error.values:
+            print()
+            for mesg in batch_exception.error.values:
+                print('{}:\t{}'.format(mesg.key, mesg.value))
+    print('-------------------------------------------')
 
 
 def wrap_commands_in_shell(ostype, commands):

Python/Batch/BatchSamples.pyproj
@@ -28,14 +28,25 @@
   <ItemGroup>
     <Compile Include="article_samples\python_tutorial_client.py" />
     <Compile Include="article_samples\python_tutorial_task.py" />
+    <Compile Include="article_samples\mpi\linux_mpi_task_demo.py" />
+    <Compile Include="article_samples\mpi\multi_task_helpers.py" />
+    <Compile Include="article_samples\mpi\azure_storage_helper.py" />
+    <Compile Include="article_samples\mpi\__init__.py" />
     <Compile Include="sample5_docker_batch_task.py" />
     <Content Include="article_samples\data\taskdata1.txt" />
     <Content Include="article_samples\data\taskdata2.txt" />
     <Content Include="article_samples\data\taskdata3.txt" />
+    <Content Include="article_samples\mpi\data\linux\openfoam\nodeprep-cmd" />
+    <Content Include="article_samples\mpi\data\linux\openfoam\coordination-cmd" />
+    <Content Include="article_samples\mpi\data\linux\openfoam\application-cmd" />
     <Content Include="resources\docker_batch_task.sh" />
     <Content Include="sample5_docker_batch_task.cfg" />
     <Folder Include="article_samples\" />
     <Folder Include="article_samples\data\" />
+    <Folder Include="article_samples\mpi" />
+    <Folder Include="article_samples\mpi\data" />
+    <Folder Include="article_samples\mpi\data\linux" />
+    <Folder Include="article_samples\mpi\data\linux\openfoam" />
     <Folder Include="common\" />
     <Folder Include="resources\" />
     <Compile Include="common\helpers.py" />

Python/Batch/article_samples/mpi/azure_storage_helper.py
@@ -1,0 +1,75 @@
+# azure_storage_helper.py - Azure storage helper to upload files to storage
+#
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from __future__ import print_function
+import argparse
+import os
+
+import azure.storage.blob as azureblob
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filepath', required=True,
+                        help='The path to the text file to process. The path '
+                             'may include a compute node\'s environment '
+                             'variables, such as '
+                             '$AZ_BATCH_NODE_SHARED_DIR/filename.txt')
+    parser.add_argument('--blobname', required=True,
+                        help='Name of blob to create or update')
+    parser.add_argument('--storageaccount', required=True,
+                        help='The name the Azure Storage account that owns '
+                             'the blob storage container to which to upload '
+                             'results.')
+    parser.add_argument('--storagecontainer', required=True,
+                        help='The Azure Blob storage container to which to '
+                             'upload results.')
+    parser.add_argument('--sastoken', required=True,
+                        help='The SAS token providing write access to the '
+                             'Storage container.')
+    args = parser.parse_args()
+
+    input_file_path = os.path.realpath(args.filepath)
+    # Create the blob client using the container's SAS token.
+    # This allows us to create a client that provides write
+    # access only to the container.
+    blob_client = azureblob.BlockBlobService(account_name=args.storageaccount,
+                                             sas_token=args.sastoken)
+
+    print(
+        'Uploading file {} container [{}], python {}/azure_storage_helper.py '
+        '--filepath {} --blobname {} --storageaccount {} --storagecontainer {}'
+        ' --sastoken ...'.format(
+            input_file_path,
+            args.storagecontainer,
+            '$AZ_BATCH_TASK_SHARED_DIR',
+            args.filepath,
+            args.blobname,
+            args.storageaccount,
+            args.storagecontainer))
+
+    blob_client.create_blob_from_path(args.storagecontainer,
+                                      args.blobname,
+                                      input_file_path)

Python/Batch/article_samples/mpi/data/linux/openfoam/application-cmd
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+#Command script to run OpenFoam application on multiple machines as MPI task on Azure Batch.
+#Usage: application [nodes] [cores_per_node] [CFD iterations]
+
+#prepare environment variables for intel mpi and openfoam
+mnt=$4
+source /opt/intel/impi/5.1.3.181/bin64/mpivars.sh
+export MPI_ROOT=$I_MPI_ROOT
+export FOAM_INST_DIR=$mnt/share/OpenFOAM
+source $mnt/share/OpenFOAM/OpenFOAM-2.3.x/etc/bashrc
+export I_MPI_FABRICS=shm:dapl
+export I_MPI_DAPL_PROVIDER=ofa-v2-ib0
+export I_MPI_DYNAMIC_CONNECTION=0
+export LD_LIBRARY_PATH=$mnt/share/OpenFOAM/intel64_lin:$LD_LIBRARY_PATH
+# total number of processes is number of nodes multipled by number of processes per node
+(( numProc = $1 * $2 ))
+
+#prepare decomposition mesh as per total number of nodes, processes and number of iterations for openfoam
+cd $mnt/share/OpenFOAM/motorBike
+echo $numProc
+sed -i.bak "s/SUB1/$numProc/g" system/decomposeParDict
+sed -i.bak2 "s/SUB2/$1/g" system/decomposeParDict
+sed -i.bak "s/SUB1/$3/g" system/controlDict
+decomposePar
+ls -d processor* | xargs -I {} rm -rf ./{}/0
+ls -d processor* | xargs -I {} cp -r 0.org ./{}/0
+
+# Run mpi application with appropriate parameters
+mpirun -np $numProc -ppn $2 --host $5 -wdir $mnt/share/OpenFOAM/motorBike simpleFoam -parallel
+
+# process the output
+reconstructPar -latestTime
+foamToVTK -latestTime
+
+# prepare the output and compress it as result
+cp $4/genimages.py VTK/
+cp $4/stdout.txt VTK/
+cp $4/stderr.txt VTK/
+cd VTK
+mv motorBike*/*.vtk .
+mmv "*:*.vtk" "#1#2.vtk"
+mmv "*%*.vtk" "#1#2.vtk"
+tar -czf $4/VTK.tgz ../VTK/

Python/Batch/article_samples/mpi/data/linux/openfoam/coordination-cmd
@@ -1,0 +1,41 @@
+#!/usr/bin/env bash
+#Command script to setup NFS share on multiple nodes (with one node being server and remaining nodes as client) as Azure Batch multiinstance task
+master_addr_port=(${AZ_BATCH_MASTER_NODE//:/ })
+master_addr=${master_addr_port[0]}
+mnt=$AZ_BATCH_TASK_SHARED_DIR
+if $AZ_BATCH_IS_CURRENT_NODE_MASTER; then
+    # is head node, will be nfs server, will download and prepare all the input data in nfs share dir
+    mkdir $mnt/share
+    chmod 777 -R $mnt/share
+    echo "$mnt/share      10.0.0.0/24(rw,sync,no_root_squash,no_all_squash)" > /etc/exports
+    systemctl restart nfs-server
+    cd $mnt/share
+    mkdir -p OpenFOAM
+    cd OpenFOAM
+    # Follow http://www.openfoam.com/code/build-guide.php to build input files for OpenFoam
+    # wget http://<storage-account>.blob.core.windows.net/<container>/OpenFOAM_CentOS7_HPC.tgz
+    # wget http://<storage-account>.blob.core.windows.net/<container>/OpenFOAM_CentOS7_HPC_libs.tgz
+    # wget http://<storage-account>.blob.core.windows.net/<container>/motorBike_3M.tgz
+    # wget http://<storage-account>.blob.core.windows.net/<container>/ParaView-3.14.1-Linux-64bit.tar.gz
+    tar zxf OpenFOAM_CentOS7_HPC.tgz
+    tar zxf OpenFOAM_CentOS7_HPC_libs.tgz
+    tar zxf motorBike_3M.tgz
+    tar zxf ParaView-3.14.1-Linux-64bit.tar.gz
+    mv motorBike_3m motorBike
+else
+    # all other nodes are nfs clients, will connect with nfs server on nfs share dir
+    mkdir -p $mnt/share
+    echo "$master_addr:$mnt/share    $mnt/share   nfs defaults 0 0::" >> /etc/fstab
+    while :
+    do
+        echo "Looping"
+        mount -a
+        mountpoint -q $mnt/share
+        if [ $? -eq 0 ]; then
+            break
+        else
+            sleep 10
+        fi
+    done
+fi
+exit

Python/Batch/article_samples/mpi/data/linux/openfoam/nodeprep-cmd
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+#Command script to run on all nodes as Azure Batch node start task, to install prerequisites etc.
+yum -y update
+yum -y install epel-release
+yum -y install python-pip
+pip install azure-storage
+yum -y install nfs-utils
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+# Disable requiretty to allow run sudo within scripts
+sed -i -e 's/Defaults    requiretty.*/ #Defaults    requiretty/g' /etc/sudoers
+rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
+yum -y install mmv

commit fc10afc16e45d09b8887713fd7ba14b956d5b56b

Fix all broken Python samples from #163

Python/Batch/common/helpers.py
@@ -341,7 +341,7 @@ def wait_for_all_nodes_state(batch_client, pool, node_state):
 
 def create_container_and_create_sas(
         block_blob_client, container_name, permission, expiry=None,
-        timeout=30):
+        timeout=None):
     """Create a blob sas token
 
     :param block_blob_client: The storage block blob client to use.
@@ -350,13 +350,15 @@ def create_container_and_create_sas(
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
     :param int timeout: timeout in minutes from now for expiry,
-                        will only be used if expiry is not specified
+        will only be used if expiry is not specified
     :return: A SAS token
     :rtype: str
     """
     if expiry is None:
-        expiry = datetime.datetime.utcnow() + \
-                 datetime.timedelta(minutes=timeout)
+        if timeout is None:
+            timeout = 30
+        expiry = datetime.datetime.utcnow() + datetime.timedelta(
+            minutes=timeout)
 
     block_blob_client.create_container(
         container_name,
@@ -368,7 +370,7 @@ def create_container_and_create_sas(
 
 def create_sas_token(
         block_blob_client, container_name, blob_name, permission, expiry=None,
-        timeout=30):
+        timeout=None):
     """Create a blob sas token
 
     :param block_blob_client: The storage block blob client to use.
@@ -378,20 +380,22 @@ def create_sas_token(
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
     :param int timeout: timeout in minutes from now for expiry,
-                        will only be used if expiry is not specified
+        will only be used if expiry is not specified
     :return: A SAS token
     :rtype: str
     """
     if expiry is None:
-        expiry = datetime.datetime.utcnow() + \
-                 datetime.timedelta(minutes=timeout)
+        if timeout is None:
+            timeout = 30
+        expiry = datetime.datetime.utcnow() + datetime.timedelta(
+            minutes=timeout)
     return block_blob_client.generate_blob_shared_access_signature(
         container_name, blob_name, permission=permission, expiry=expiry)
 
 
 def upload_blob_and_create_sas(
         block_blob_client, container_name, blob_name, file_name, expiry,
-        timeout):
+        timeout=None):
     """Uploads a file from local disk to Azure Storage and creates
     a SAS for it.
 
@@ -403,7 +407,7 @@ def upload_blob_and_create_sas(
     :param expiry: The SAS expiry time.
     :type expiry: `datetime.datetime`
     :param int timeout: timeout in minutes from now for expiry,
-                        will only be used if expiry is not specified
+        will only be used if expiry is not specified
     :return: A SAS URL to the blob with the specified expiry time.
     :rtype: str
     """
@@ -432,8 +436,8 @@ def upload_blob_and_create_sas(
     return sas_url
 
 
-def upload_file_to_container(block_blob_client, container_name, file_path,
-                             timeout):
+def upload_file_to_container(
+        block_blob_client, container_name, file_path, timeout):
     """
     Uploads a local file to an Azure Blob storage container.
 
@@ -442,32 +446,30 @@ def upload_file_to_container(block_blob_client, container_name, file_path,
     :param str container_name: The name of the Azure Blob storage container.
     :param str file_path: The local path to the file.
     :param int timeout: timeout in minutes from now for expiry,
-                        will only be used if expiry is not specified
+        will only be used if expiry is not specified
     :rtype: `azure.batch.models.ResourceFile`
     :return: A ResourceFile initialized with a SAS URL appropriate for Batch
     tasks.
     """
     blob_name = os.path.basename(file_path)
-    print('Uploading file {} to container [{}]...'.format(file_path,
-                                                          container_name))
-    sas_url = upload_blob_and_create_sas(block_blob_client, container_name,
-                                         blob_name, file_path, expiry=None,
-                                         timeout=timeout)
-    return batchmodels.ResourceFile(file_path=blob_name,
-                                    blob_source=sas_url)
+    print('Uploading file {} to container [{}]...'.format(
+        file_path, container_name))
+    sas_url = upload_blob_and_create_sas(
+        block_blob_client, container_name, blob_name, file_path, expiry=None,
+        timeout=timeout)
+    return batchmodels.ResourceFile(
+        file_path=blob_name, blob_source=sas_url)
 
 
-def download_blob_from_container(block_blob_client,
-                                 container_name,
-                                 blob_name,
-                                 directory_path):
+def download_blob_from_container(
+        block_blob_client, container_name, blob_name, directory_path):
     """
     Downloads specified blob from the specified Azure Blob storage container.
 
     :param block_blob_client: A blob service client.
     :type block_blob_client: `azure.storage.blob.BlockBlobService`
     :param container_name: The Azure Blob storage container from which to
-     download file.
+        download file.
     :param blob_name: The name of blob to be downloaded
     :param directory_path: The local directory to which to download the file.
     """
@@ -476,14 +478,11 @@ def download_blob_from_container(block_blob_client,
 
     destination_file_path = os.path.join(directory_path, blob_name)
 
-    block_blob_client.get_blob_to_path(container_name,
-                                       blob_name,
-                                       destination_file_path)
+    block_blob_client.get_blob_to_path(
+        container_name, blob_name, destination_file_path)
 
     print('  Downloaded blob [{}] from container [{}] to {}'.format(
-         blob_name,
-         container_name,
-         destination_file_path))
+        blob_name, container_name, destination_file_path))
 
     print('  Download complete!')
 
@@ -538,9 +537,8 @@ def print_batch_exception(batch_exception):
     """
     print('-------------------------------------------')
     print('Exception encountered:')
-    if batch_exception.error and \
-            batch_exception.error.message and \
-            batch_exception.error.message.value:
+    if (batch_exception.error and batch_exception.error.message and
+            batch_exception.error.message.value):
         print(batch_exception.error.message.value)
         if batch_exception.error.values:
             print()

Python/Batch/sample4_docker_swarm.py
@@ -53,7 +53,8 @@ _SSH_TUNNEL_SCRIPT = 'ssh_tunnel_batch_docker_swarm.sh'
 def connect_to_remote_docker_swarm_master(
         batch_client, pool_id, nodes, master_node_id, username,
         ssh_private_key, generate_ssh_tunnel_script):
-    """Adds an administrative user to the Batch Compute Node
+    """Connects to the remote docker swarm master to run sample commands and
+    optionally generates an ssh tunnel script
 
     :param batch_client: The batch client to use.
     :type batch_client: `batchserviceclient.BatchServiceClient`