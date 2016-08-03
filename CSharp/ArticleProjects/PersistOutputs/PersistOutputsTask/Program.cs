1bef33351066f67b790f4f4ec4b69b5df93b0ebb
Marsh Macy <marsma@microsoft.com>
2016-08-02 17:09:20 -0700

    Add output persistence article sample project

---
+++ CSharp/ArticleProjects/PersistOutputs/PersistOutputsTask/Program.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Microsoft Corporation
+//
+// Companion project to the following article:
+// https://azure.microsoft.com/documentation/articles/ARTICLE-SLUG-HERE/
+
+namespace Microsoft.Azure.Batch.Samples.Articles.PersistOutputs.PersistOutputsTask
+{
+    using Microsoft.Azure.Batch.Conventions.Files;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    // This sample task application creates a million random numbers and writes them to a file,
+    // pretending they're the results of Monte Carlo trials. The detailed results file is saved
+    // as a task OUTPUT, and creates a file containing just the mean and standard deviation which
+    // is saved as a task PREVIEW.
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Obtain service-defined environment variables
+            string jobId = Environment.GetEnvironmentVariable("AZ_BATCH_JOB_ID");
+            string taskId = Environment.GetEnvironmentVariable("AZ_BATCH_TASK_ID");
+
+            // Obtain the custom environment variable we set in the client application
+            string jobContainerUrl = Environment.GetEnvironmentVariable("JOB_CONTAINER_URL");
+
+            // The task will use the TaskOutputStorage to store both its output and log updates
+            TaskOutputStorage taskStorage = new TaskOutputStorage(new Uri(jobContainerUrl), taskId);
+
+            // The primary task logic is wrapped in a using statement that sends updates to stdout.txt
+            // in Storage every 15 seconds while the task code runs.
+            using (Task<ITrackedSaveOperation> stdout = taskStorage.SaveTrackedAsync(
+                TaskOutputKind.TaskLog, 
+                RootDir("stdout.txt"), 
+                "stdout.txt", 
+                TimeSpan.FromSeconds(15)))
+            {
+                string outputFile = $"results_{taskId}.txt";
+                string summaryFile = $"summary_{taskId}.txt";
+
+                using (StreamWriter output = File.CreateText(WorkingDir(outputFile)))
+                {
+                    using (StreamWriter summary = File.CreateText(WorkingDir(summaryFile)))
+                    {
+                        output.WriteLine($"# Task {taskId}");
+
+                        int runCount = 1000000;
+                        int[] results = new int[runCount];
+                        double resultTotal = 0;
+
+                        for (int i = 0; i < runCount; ++i)
+                        {
+                            var runResult = PerformSingleRunMonteCarloSimulation();
+                            output.WriteLine($"{i}, {runResult}");
+                            results[i] = runResult;
+                            resultTotal += runResult;
+
+                            if (i % 5000 == 0)
+                            {
+                                Console.WriteLine($"{DateTime.UtcNow}: Processing... done {i}");
+                            }
+                        }
+
+                        double mean = resultTotal / runCount;
+                        double stddev = Math.Sqrt((from r in results
+                                                let d = r - mean
+                                                select d * d).Average());
+
+                        summary.WriteLine($"Task:      {taskId}");
+                        summary.WriteLine($"Run count: {runCount}");
+                        summary.WriteLine($"Mean:      {mean}");
+                        summary.WriteLine($"Std dev:   {stddev}");
+                    }
+                }
+
+                // Persist the task output to Azure Storage
+                Task.WaitAll(
+                    taskStorage.SaveAsync(TaskOutputKind.TaskOutput, outputFile),
+                    taskStorage.SaveAsync(TaskOutputKind.TaskPreview, summaryFile)
+                    );
+
+                return 0;
+            }
+        }
+
+        private static string RootDir(string path)
+        {
+            return Path.Combine(Environment.GetEnvironmentVariable("AZ_BATCH_TASK_DIR"), path);
+        }
+
+        private static string WorkingDir(string path)
+        {
+            return Path.Combine(Environment.GetEnvironmentVariable("AZ_BATCH_TASK_WORKING_DIR"), path);
+        }
+
+        private static readonly Random _random = new Random();
+
+        private static int PerformSingleRunMonteCarloSimulation()
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(0.1));
+            return _random.Next(100);
+        }
+    }
+}
---
+++ CSharp/ArticleProjects/PersistOutputs/Program.cs
@@ -0,0 +1,182 @@
+﻿// Copyright (c) Microsoft Corporation
+//
+// Companion project to the following article:
+// https://azure.microsoft.com/documentation/articles/batch-task-output/
+
+namespace Microsoft.Azure.Batch.Samples.Articles.PersistOutputs
+{
+    using Microsoft.Azure.Batch;
+    using Microsoft.Azure.Batch.Auth;
+    using Microsoft.Azure.Batch.Conventions.Files;
+    using Microsoft.Azure.Batch.Samples.Common;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Auth;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configure your AccountSettings in the Microsoft.Azure.Batch.Samples.Common project within this solution
+            BatchSharedKeyCredentials cred = new BatchSharedKeyCredentials(AccountSettings.Default.BatchServiceUrl,
+                                                                           AccountSettings.Default.BatchAccountName,
+                                                                           AccountSettings.Default.BatchAccountKey);
+
+            StorageCredentials storageCred = new StorageCredentials(AccountSettings.Default.StorageAccountName,
+                                                                    AccountSettings.Default.StorageAccountKey);
+
+            string jobId = Guid.NewGuid().ToString();
+            string poolId = "PersistOutputsSamplePool";
+            int nodeCount = 3;
+            string appPackageId = "PersistOutputTask";
+            string appPackageVersion = "1.1";
+
+            using (BatchClient batchClient = BatchClient.Open(cred))
+            {
+                // Create and configure an unbound pool.
+                CloudPool pool = batchClient.PoolOperations.CreatePool(poolId: poolId,
+                    virtualMachineSize: "small",
+                    targetDedicated: nodeCount,
+                    cloudServiceConfiguration: new CloudServiceConfiguration("4"));
+
+                // Specify the application and version to deploy to the compute nodes
+                pool.ApplicationPackageReferences = new List<ApplicationPackageReference>
+                {
+                    new ApplicationPackageReference {
+                        ApplicationId = appPackageId,
+                        Version = appPackageVersion }
+                };
+
+                // Commit the pool to the Batch service
+                pool.Commit();
+
+
+                CloudJob job = batchClient.JobOperations.CreateJob(jobId, new PoolInformation { PoolId = poolId });
+
+                CloudStorageAccount linkedStorageAccount = new CloudStorageAccount(storageCred, true);
+
+                // Create the blob storage container for the outputs.
+                job.PrepareOutputStorageAsync(linkedStorageAccount).Wait();
+
+                // Create an environment variable on the compute nodes that the
+                // task application can reference when persisting its outputs.
+                string containerName = job.OutputStorageContainerName();
+                CloudBlobContainer container = linkedStorageAccount.CreateCloudBlobClient().GetContainerReference(containerName);
+                string containerSas = container.GetSharedAccessSignature(FullAccessPolicy());
+                string containerUrl = container.Uri.AbsoluteUri + containerSas;
+                job.CommonEnvironmentSettings = new[] { new EnvironmentSetting("JOB_CONTAINER_URL", containerUrl) };
+
+                // Commit the job to the Batch service
+                job.Commit();
+                Console.WriteLine($"Created job {jobId}");
+
+                // Obtain the bound job from the Batch service
+                job = batchClient.JobOperations.GetJob(jobId);
+
+
+                IEnumerable<CloudTask> tasks = Enumerable.Range(1, 20).Select(i =>
+                    new CloudTask(i.ToString().PadLeft(3, '0'), $"cmd /c %AZ_BATCH_APP_PACKAGE_{appPackageId.ToUpper()}#{appPackageVersion}%\\PersistOutputsTask.exe")
+                );
+
+                // Add the tasks to the job; the tasks are automatically
+                // scheduled for execution on the nodes by the Batch service.
+                job.AddTask(tasks);
+
+                Console.WriteLine($"All tasks added to job {job.Id}");
+                Console.WriteLine();
+
+                foreach (CloudTask task in CompletedTasks(batchClient, job))
+                {
+                    Console.Write($"Task {task.Id} completed, ");
+                    foreach (OutputFileReference output in task.OutputStorage(linkedStorageAccount).ListOutputs(TaskOutputKind.TaskOutput))
+                    {
+                        Console.WriteLine($"output file: {output.FilePath}");
+                        output.DownloadToFileAsync($"{jobId}-{output.FilePath}", System.IO.FileMode.Create).Wait();
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("All tasks completed, you may now view the outputs in the Azure portal.");
+
+                // Clean up the resources we've created (job, pool, and blob storage container)
+                Console.WriteLine();
+                Console.Write("Delete job? [yes] no: ");
+                string response = Console.ReadLine().ToLower();
+                if (response != "n" && response != "no")
+                {
+                    batchClient.JobOperations.DeleteJob(job.Id);
+                }
+
+                Console.Write("Delete pool? [yes] no: ");
+                response = Console.ReadLine().ToLower();
+                if (response != "n" && response != "no")
+                {
+                    batchClient.PoolOperations.DeletePool(poolId);
+                }
+
+                Console.Write("Delete storage container? [yes] no: ");
+                response = Console.ReadLine().ToLower();
+                if (response != "n" && response != "no")
+                {
+                    container.Delete();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Sample complete, hit ENTER to exit...");
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Monitors the specified job's tasks and returns each as they complete. When all
+        /// of the tasks in the job have completed, the method returns.
+        /// </summary>
+        /// <param name="client">The <see cref="<see cref="BatchClient"/>" for the account containing the job.</param>
+        /// <param name="job">The <see cref="CloudJob"/> containing the tasks to monitor.</param>
+        /// <returns>One or more completed <see cref="CloudTask"/>.</returns>
+        private static IEnumerable<CloudTask> CompletedTasks(BatchClient client, CloudJob job)
+        {
+            HashSet<string> yieldedTasks = new HashSet<string>();
+
+            while (true)
+            {
+                List<CloudTask> tasks = job.ListTasks().ToList();
+
+                List<CloudTask> newlyCompleted = tasks.Where(t => t.State == Microsoft.Azure.Batch.Common.TaskState.Completed)
+                                          .Where(t => !yieldedTasks.Contains(t.Id))
+                                          .ToList();
+
+                foreach (var task in newlyCompleted)
+                {
+                    yield return task;
+                    yieldedTasks.Add(task.Id);
+                }
+
+                if (yieldedTasks.Count == tasks.Count)
+                {
+                    yield break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a <see cref="SharedAccessBlobPolicy"/> with full CRUD permissions, valid for 1 day.
+        /// </summary>
+        /// <returns>Full-access <see cref="SharedAccessBlobPolicy"/>.</returns>
+        private static SharedAccessBlobPolicy FullAccessPolicy()
+        {
+            return new SharedAccessBlobPolicy
+            {
+                Permissions = SharedAccessBlobPermissions.Add |
+                              SharedAccessBlobPermissions.Create |
+                              SharedAccessBlobPermissions.List |
+                              SharedAccessBlobPermissions.Read |
+                              SharedAccessBlobPermissions.Write,
+                SharedAccessExpiryTime = DateTime.UtcNow.AddDays(1),
+            };
+        }
+    }
+}
