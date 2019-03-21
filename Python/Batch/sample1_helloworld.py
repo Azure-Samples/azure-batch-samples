493429ae3c377cf366f2954897b48ba8ab1cd6f6
Michal Kostewicz <m.kostewicz84@gmail.com>
2019-03-21 22:48:24 +0100

    Change of the name of the BatchServiceClient class parameter to the one consistent with the documentation. From `base_url` -> `batch_url`. Wrong name caused the error at run time: ``` TypeError: __init__() got an unexpected keyword argument 'base_url' ```

--- Python/Batch/sample1_helloworld.py
+++ Python/Batch/sample1_helloworld.py
@@ -105,7 +105,7 @@ def execute_sample(global_config, sample_config):
 
     batch_client = batch.BatchServiceClient(
         credentials,
-        base_url=batch_service_url)
+        batch_url=batch_service_url)
 
     # Retry 5 times -- default is 3
     batch_client.config.retry_policy.retries = 5
