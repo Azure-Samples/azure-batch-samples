ce1a53a59f8786c375614a20b78a242f7285a277
Xing Wu <xingwu@microsoft.com>
2019-09-06 14:46:05 -0700

    Update the Node.Js sample to latest SDK. (#282)

--- Node.js/GettingStarted/nodejs_batch_client_sample.js
+++ Node.js/GettingStarted/nodejs_batch_client_sample.js
@@ -1,6 +1,5 @@
 // Importing required node.js modules
 var batch = require('azure-batch');
-var uuid = require('node-uuid');
 
 // Replace values below with Batch Account details 
 var accountName = '<batch-account-name>';
@@ -8,10 +7,10 @@ var accountKey = '<batch-account-key>';
 var accountUrl = '<batch-account-url>';
 
 // Replace values with SAS URIs of the shell script file
-var sh_url = "https://batchdevsgsa.blob.core.windows.net/downloads/startup_prereq.sh?st=2017-04-10T18%3A11%3A00Z&se=2018-03-11T18%3A11%3A00Z&sp=rl&sv=2015-12-11&sr=b&sig=TEGlekTbAlGAqInp3m4ZEAAyHdE5NwGQQaJVmxcg5AE%3D";
+var sh_url = "https://batchdevsgsa.blob.core.windows.net/downloads/startup_prereq.sh?st=2017-04-10T18%3A11%3A00Z&se=2020-03-11T18%3A11%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=xxxx";
 
 // Replace values with SAS URIs of the Python script file
-var scriptURI = "https://batchdevsgsa.blob.core.windows.net/downloads/processcsv.py?st=2017-04-10T18%3A11%3A00Z&se=2018-03-11T18%3A11%3A00Z&sp=rl&sv=2015-12-11&sr=b&sig=ngeN60AzqSc%2Bugx2H7XUbiNXysb1%2BMvuW%2Fk8FhsU3KU%3D";
+var scriptURI = "https://batchdevsgsa.blob.core.windows.net/downloads/processcsv.py?st=2017-04-10T18%3A11%3A00Z&se=2020-03-11T18%3A11%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=xxxx";
 
 // Pool ID 
 var now = new Date();
@@ -29,20 +28,20 @@ var batchClient = new batch.ServiceClient(credentials,accountUrl);
 // Pool VM Image Reference
 var imgRef = {publisher:"Canonical",
                 offer:"UbuntuServer",
-                sku:"14.04.2-LTS",
+                sku:"16.04-LTS",
                 version:"latest"}
 // Pool VM configuraion object
 var vmConfig = {imageReference:imgRef,
-    nodeAgentSKUId:"batch.node.ubuntu 14.04"};
+    nodeAgentSKUId:"batch.node.ubuntu 16.04"};
 // Number of VMs to create in a pool
 var numVms = 4;
-var vmSize = "STANDARD_A1";
+var vmSize = "STANDARD_D1_V2";
 // Pool configuration object
 var poolConfig = {id:poolId, 
     displayName:"Processing csv files",
     vmSize:vmSize,
     virtualMachineConfiguration:vmConfig,
-    targetDedicated:numVms,
+    targetDedicatedNodes:numVms,
     enableAutoScale:false };
 
 // Creating Batch Pool
@@ -67,7 +66,7 @@ function createJob()
     // Preparation Task configuraion object    
     var jobPrepTaskConfig = {id:"installprereq",
         commandLine:"sudo sh startup_prereq.sh > startup.log",
-        resourceFiles:[{'blobSource':sh_url,'filePath':'startup_prereq.sh'}],
+        resourceFiles:[{'httpUrl':sh_url,'filePath':'startup_prereq.sh'}],
         waitForSuccess:true,runElevated:true};
 
     // Setting Batch Pool ID
@@ -106,7 +105,7 @@ function createTasks()
         var taskConfig = {id:taskID,
             displayName:'process csv in ' + containerName,
             commandLine:'python processcsv.py --container ' + containerName,
-            resourceFiles:[{'blobSource':scriptURI,'filePath':'processcsv.py'}]};
+            resourceFiles:[{'httpUrl':scriptURI,'filePath':'processcsv.py'}]};
 
         var task = batchClient.task.add(jobId,taskConfig,function(error,result){
             if(error !== null)
